4338dabe366cea11ccce0c4b2d0a1c7c69fd010b
adbtzmi <adibtazmi2003@gmail.com>
2025-02-05 02:33:35 +0800

    .

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -8,7 +8,7 @@ const TAB_DATA = [
     title: "Skills",
     id: "skills",
     content: (
-      <div className="grid grid-cols-1 md:grid-cols-2 ml-2">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-2 ml-2">
         <ul className="list-disc pl-2">
           <li>HTML</li>
           <li>CSS</li>
@@ -64,7 +64,7 @@ const AboutSection = () => {
   return (
     <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/images/about-image.png" width={500} height={500} className="rounded-lg" />
+        <Image src="/images/about-image.png" width={500} height={500} className="rounded-lg" alt="About Image" />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
