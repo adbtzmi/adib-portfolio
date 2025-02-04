ecae8135ab0b69242892ad005608304d27f8b07c
adbtzmi <adibtazmi2003@gmail.com>
2025-02-05 01:48:35 +0800

    .

--- src/app/components/AchievementsSection.jsx
+++ src/app/components/AchievementsSection.jsx
@@ -30,24 +30,37 @@ const achievementsList = [
 
 const AchievementsSection = () => {
   return (
-    <section className="py-12">
-      <div className="container mx-auto">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
-          {achievementsList.map((achievement, index) => (
+    <div className="py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+      <div className="sm:border-[#33353F] sm:border rounded-md py-8 px-16 flex flex-col sm:flex-row items-center justify-between">
+        {achievementsList.map((achievement, index) => {
+          return (
             <div
               key={index}
-              className="flex flex-col items-center justify-center text-center bg-gray-800 p-6 rounded-lg"
+              className="flex flex-col items-center justify-center mx-4 my-4 sm:my-0"
             >
-              <AnimatedNumbers
-                animateToNumber={parseInt(achievement.value)}
-                fontStyle={{ fontSize: 40, color: "white" }}
-              />
-              <p className="text-white mt-2">{achievement.metric}</p>
+              <h2 className="text-white text-4xl font-bold flex flex-row">
+                {achievement.prefix}
+                <AnimatedNumbers
+                  includeComma
+                  animateToNumber={parseInt(achievement.value)}
+                  locale="en-US"
+                  className="text-white text-4xl font-bold"
+                  configs={(_, index) => {
+                    return {
+                      mass: 1,
+                      friction: 100,
+                      tensions: 140 * (index + 1),
+                    };
+                  }}
+                />
+                {achievement.postfix}
+              </h2>
+              <p className="text-[#ADB7BE] text-base text-center">{achievement.metric}</p>
             </div>
-          ))}
-        </div>
+          );
+        })}
       </div>
-    </section>
+    </div>
   );
 };
 
